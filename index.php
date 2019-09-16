e041b6ee1599bc32fffc341efe25fc223f29cc95
LucaPanas <49339752+elpanas@users.noreply.github.com>
2019-09-16 12:19:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -36,9 +36,15 @@ switch ($content["azione"])
     $risposta[0]["nome"] = "Lido Nettuno";
     $risposta[0]["indirizzo"] = "Lungomare manfredonia";
     $risposta[0]["localita"] = "Siponto";
-    $risposta[0]["ombrelloni"] = 186;  
-    echo json_encode($risposta);
+    $risposta[0]["ombrelloni"] = 186;      
+    break;
+  case "stabilimento":
+    $risposta[0]["id"] = 1;
+    $risposta[0]["nome"] = "Lido Nettuno";
+    $risposta[0]["indirizzo"] = "Lungomare manfredonia";
+    $risposta[0]["localita"] = "Siponto";
+    $risposta[0]["ombrelloni"] = 185; 
     break;
 }
-
+echo json_encode($risposta);
 ?>
