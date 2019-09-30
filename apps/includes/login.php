9472bd6e6b96458180cd934133f126493f96c0c7
Luca Panariello <l.panariello@campus.uniurb.it>
2019-09-30 22:43:21 +0200

    test

--- apps/includes/login.php
+++ apps/includes/login.php
@@ -4,7 +4,7 @@ $dati = loginUtente($db,$content['username'],$content['password']);
 /*
 if ($dati != null)
     inserisceSessione($db,$dati['idu']);  
-else
-    $dati['idu'] = 0;
-   */
+else*/
+    $dati['idu'] = 1;
+   
 $risposta = $dati;
