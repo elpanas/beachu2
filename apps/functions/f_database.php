b3d1c5b51a7bbb486e57324bc44affbd99433c61
Luca Panariello <l.panariello@campus.uniurb.it>
2019-10-02 15:52:29 +0200

    mods

--- apps/functions/f_database.php
+++ apps/functions/f_database.php
@@ -40,7 +40,7 @@ function cercaStabilimenti($db,
     {
         $lat = $content['latitudine'];
         $long = $content['longitudine'];
-        $where = "(SQRT( (POW(latitudine,2) - POW($lat,2)) + (POW(longitudine,2) - POW($long,2)))) <= 3";        
+        $where = "(SQRT( (POW(latitudine,2) - POW($lat,2)) + (POW(longitudine,2) - POW($long,2)))) <= 3000";        
     }
         
     $elenco = null; // output: dati degli stabilimenti 
