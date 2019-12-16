47b60ca2e238df7398fd067867bc98c1755fe9a3
Luca Panariello <l.panariello@campus.uniurb.it>
2019-12-16 01:04:17 +0100

    mods

--- apps/functions/f_database.php
+++ apps/functions/f_database.php
@@ -252,7 +252,7 @@ function loginUtente($db,      // input: oggetto per comunicare col database
                      $user,    // input: username telegram  
                      $psw) {                    
 
-    $dati = null; // output: array associativo con i dati
+    $dati['Id'] = 0; // output: array associativo con i dati
     
     $query = "SELECT id                                       
               FROM utenti
